492dcf8b427995ee4a2f7ebd600bdb351ffe2f49
Jordyn Anderson <jordyna@spruce.rlogin>
2017-12-06 13:53:08 -0500

    get data from the joe info file. USE CAPS

--- sanitize.pl
+++ sanitize.pl
@@ -1,4 +1,4 @@
-%to be completed
+% to be completed
 			      
 
 isProblematic(S) :-
@@ -7,9 +7,19 @@ isProblematic(S) :-
                   sub_string(S, _, _, _, 'snorted');
                   sub_string(S, _, _, _, 'drinking').
 
-rule2Post(ID, user) :- waslike(ID, number), friends(user, amount), number < amount * (2 / 10).
+postProb(e) :- isProblematic(e).
+
+% rule2Post(ID, User) :- 
+					waslike(ID, Number), 
+					friends(User, Amount), 
+					Number < Amount * (2 / 10).
+
+findFriends(User, Person):- friends(User, Person).
+
+findPost(ID, user, message) :- post(ID, user, message).
 
 % the final rule to build
-% toBeCheckedPost(User, IntViewer, ID) :- ...
+toBeCheckedPost(User, IntViewer, ID) :- 
+										friends(User, pete) , friends(User, ali).
 
-%setof(ID, toBeCheckedPost(joe, rick, ID), S).
+% setof(ID, toBeCheckedPost(joe, rick, ID), S).
