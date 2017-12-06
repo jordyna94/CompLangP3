f748d346f011313d0fd09f19ae75fe8976c8f0b6
Jordyn Anderson <jordyna@sassafras.rlogin>
2017-12-06 16:39:10 -0500

    Rule 2 completed

--- sanitize.pl
+++ sanitize.pl
@@ -7,24 +7,28 @@ isProblematic(S) :-
                   sub_string(S, _, _, _, 'snorted');
                   sub_string(S, _, _, _, 'drinking').
 
-postProb(e) :- isProblematic(e).
-
-% rule2Post(ID, User) :- 
-					waslike(ID, Number), 
-					friends(User, Amount), 
-					Number < Amount * (2 / 10).
-
-findFriends(User, Person):- friends(User, Person).
+% Rule 2. The number of likes is less than 20% of the friends a user has
+likesOverMax(ID, User) :- 
+					aggregate_all(count,like(_, ID) ,Likes),
+					friends(User, People),
+					length_1(X, People), 
+					Friends is X, 
+					V is 2/10, 
+					P is V * Friends, 
+					Likes < P.
 
 isFriend(User, Friend) :- friends(User, P), memberchk(Friend, P).
+
+% Get the size of a list
+length_1(0, []).
+length_1(L+1, [H|T]) :- length_1(L,T).
 
 friendsCount(User, List) :- Length(List, X).
 
 findPost(ID, user, message) :- post(ID, user, message).
 
 % the final rule to build
-toBeCheckedPost(User, IntViewer, ID) :- 
-										friends(User, pete) , friends(User, ali).
+toBeCheckedPost(User, IntViewer, ID) :- ...
 
 % setof(ID, toBeCheckedPost(joe, rick, ID), S).
 
